f6106fcfc789ac021c7c82a29922ae33d542f01f
s4ndalhat <winness.it@gmail.com>
2024-10-15 12:28:08 +0200

    feat: use BUFFER_SIZE constant

--- 01-syscalls_fs/src/copy.c
+++ 01-syscalls_fs/src/copy.c
@@ -3,20 +3,20 @@
 void copy(const char *src, const char *dst)
 {
   int src_fd, dst_fd;
-  char buffer[4096];
+  char buffer[BUFFER_SIZE];
   ssize_t bytes_read, bytes_written;
 
   src_fd = open(src, O_RDONLY);
   if (src_fd == -1)
   {
-    perror("Failed to open src file");
+    print_error("Failed to open src file\n");
     exit(EXIT_FAILURE);
   }
 
   dst_fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
   if (dst_fd == -1)
   {
-    perror("Failed to open dst file");
+    print_error("Failed to open dst file\n");
     close(src_fd);
     exit(EXIT_FAILURE);
   }
@@ -26,7 +26,7 @@ void copy(const char *src, const char *dst)
     bytes_written = write(dst_fd, buffer, bytes_read);
     if (bytes_written != bytes_read)
     {
-      perror("Failed to write to dst file");
+      print_error("Failed to write to dst file\n");
       close(src_fd);
       close(dst_fd);
       exit(EXIT_FAILURE);
@@ -34,7 +34,7 @@ void copy(const char *src, const char *dst)
   }
 
   if (bytes_read == -1)
-    perror("Failed to read from src file");
+    print_error("Failed to read from src file\n");
 
   close(src_fd);
   close(dst_fd);
