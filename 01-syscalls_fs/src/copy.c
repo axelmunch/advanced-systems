1385399e67719ea0f30b494ecabbaeefe3cc7755
s4ndalhat <winness.it@gmail.com>
2024-10-16 08:58:36 +0200

    feat: add logging verbose in copy command

--- 01-syscalls_fs/src/copy.c
+++ 01-syscalls_fs/src/copy.c
@@ -2,39 +2,49 @@
 
 void copy(const char *src, const char *dst) 
 {
+    print("[+] Copying %s to %s\n", src, dst);
     int src_fd, dst_fd;
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read, bytes_written;
 
+    print("[+] Opening %s file for reading...\n", src);
     src_fd = open(src, O_RDONLY);
     if (src_fd == -1) 
     {
-        perror("Failed to open src file\n");
+        perror("[!] Failed to open source file");
         exit(EXIT_FAILURE);
     }
+    print("[+] Source file opened successfully\n");
 
+    print("[+] Opening %s file for writing...\n", dst);
     dst_fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (dst_fd == -1) 
     {
-        perror("Failed to open dst file");
+        perror("[!] Failed to open destination file");
         close(src_fd);
         exit(EXIT_FAILURE);
     }
+    print("[+] Destination file opened successfully\n");
 
-    while ((bytes_read = read(src_fd, buffer, sizeof(buffer))) > 0)
+    print("[+] Copying data from %s to %s...\n", src, dst);
+    size_t buffer_size = sizeof(buffer);
+    while ((bytes_read = read(src_fd, buffer, buffer_size)) > 0)
     {
         bytes_written = write(dst_fd, buffer, bytes_read);
         if (bytes_written != bytes_read) 
         {
-            perror("Failed to write to dst file");
+            perror("[!] Error writing to destination file");
             close(src_fd);
             close(dst_fd);
             exit(EXIT_FAILURE);
         }
+        print("[DEBUG] Wrote %ld bytes to destination\n", bytes_written);
     }
 
     if (bytes_read == -1)
-        perror("Failed to read from src file");
+        perror("[!] Error reading from source file");
+
+    print("[+] File copy completed successfully\n");
 
     close(src_fd);
     close(dst_fd);
