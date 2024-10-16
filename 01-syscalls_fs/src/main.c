eea59af9cd5abfd868352e6c6b191ae945751dbb
s4ndalhat <winness.it@gmail.com>
2024-10-16 16:22:38 +0200

    feat: clean main loop binary

--- 01-syscalls_fs/src/main.c
+++ 01-syscalls_fs/src/main.c
@@ -3,12 +3,10 @@
 #include <string.h>
 #include <errno.h>
 #include <stdbool.h>
-
 #include <getopt.h>
 #include "constants.h"
 #include "print.h"
 #include "copy.h"
-
 
 /**
  * Procedure checks if variable must be free
@@ -18,142 +16,138 @@
  * \see man 3 free
  * \return void
  */
-void free_if_needed(void* to_free)
+void free_if_needed(void *to_free) 
 {
-    if (to_free != NULL) free(to_free);
+    if (to_free != NULL)
+        free(to_free);
 }
-
 
 /**
- *
- * \see man 3 strndup
- * \see man 3 perror
- * \return
+ * Duplicate the option argument string (optarg)
  */
-char* dup_optarg_str()
+char *dup_optarg_str() 
 {
-    char* str = NULL;
-
-    if (optarg != NULL)
+    char *str = NULL;
+    if (optarg != NULL) 
     {
         str = strndup(optarg, MAX_PATH_LENGTH);
-
-        // Checking if ERRNO is set
-        if (str == NULL)
+        if (str == NULL) 
+        {
             perror(strerror(errno));
+        }
     }
-
     return str;
 }
-
 
 /**
  * Binary options declaration
  * (must end with {0,0,0,0})
- *
- * \see man 3 getopt_long or getopt
- * \see struct option definition
  */
-static struct option binary_opts[] =
-{
-    { "help",    no_argument,       0, 'h' },
-    { "verbose", no_argument,       0, 'v' },
-    { "input",   required_argument, 0, 'i' },
-    { "output",  required_argument, 0, 'o' },
-    { 0,         0,                 0,  0  }
+static struct option binary_opts[] = {
+    {"help",    no_argument,       0, 'h'},
+    {"verbose", no_argument,       0, 'v'},
+    {"input",   required_argument, 0, 'i'},
+    {"output",  required_argument, 0, 'o'},
+    {0,         0,                 0,  0}
 };
 
 /**
- * Binary options string
- * (linked to optionn declaration)
- *
- * \see man 3 getopt_long or getopt
+ * Struct to store binary parameters
  */
-const char* binary_optstr = "hvi:o:";
+typedef struct {
+    char *input;
+    char *output;
+} binary_params_t;
 
+/**
+ * Checking binary requirements
+ * (could be defined in a separate function)
+ */
+void check_requirements(binary_params_t *params) 
+{
+    if (params->input == NULL || params->output == NULL) 
+    {
+        print_error("Bad usage! See HELP [--help|-h]\n");
+        free_if_needed(params->input);
+        free_if_needed(params->output);
+        exit(EXIT_FAILURE);
+    }
+}
 
+/**
+ * Function to print the binary parameters
+ */
+void print_parameters(const binary_params_t *params, bool verbose_mode) 
+{
+    print("** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n",
+          "input", params->input,
+          "output", params->output,
+          "verbose", verbose_mode);
+}
+
+/**
+ * Print help and exit
+ */
+void print_help(char **argv, binary_params_t *params) 
+{
+    print_generic(STDOUT, "USAGE: %s %s\n\n%s\n", argv[0], USAGE_SYNTAX, USAGE_PARAMS);
+    free_if_needed(params->input);
+    free_if_needed(params->output);
+    exit(EXIT_SUCCESS);
+}
 
 /**
  * Binary main loop
  *
- * \return 1 if it exit successfully
+ * \return 1 if it exits successfully
  */
-int main(int argc, char** argv)
+int main(int argc, char **argv) 
 {
-    /**
-     * Binary variables
-     * (could be defined in a structure)
-     */
-    char* bin_input_param = NULL;
-    char* bin_output_param = NULL;
+    // Binary parameters initialization
+    binary_params_t params = {0};
 
     // Parsing options
     int opt = -1;
     int opt_idx = -1;
 
-    while ((opt = getopt_long(argc, argv, binary_optstr, binary_opts, &opt_idx)) != -1)
+    while ((opt = getopt_long(argc, argv, "hvi:o:", binary_opts, &opt_idx)) != -1) 
     {
-        switch (opt)
+        switch (opt) 
         {
-            case 'i':
+        case 'i':
             // Input param
-            if (optarg)
-            {
-                bin_input_param = dup_optarg_str();
-            }
+            if (optarg) 
+                params.input = dup_optarg_str();
             break;
-            case 'o':
+        case 'o':
             // Output param
             if (optarg)
-            {
-                bin_output_param = dup_optarg_str();
-            }
+                params.output = dup_optarg_str();
             break;
-            case 'v':
+        case 'v':
             // Verbose mode
             set_verbose_mode(true);
             break;
-            case 'h':
-            print("USAGE: %s %s\n\n%s\n", argv[0], USAGE_SYNTAX, USAGE_PARAMS);
-
-            free_if_needed(bin_input_param);
-            free_if_needed(bin_output_param);
-
-            exit(EXIT_SUCCESS);
-            default :
+        case 'h':
+            print_help(argv, &params);
+            break;
+        default:
             break;
         }
     }
 
-    /**
-     * Checking binary requirements
-     * (could defined in a separate function)
-     */
-    if (bin_input_param == NULL || bin_output_param == NULL)
-    {
-        print_error("Bad usage! See HELP [--help|-h]\n");
-        // Freeing allocated data
-        free_if_needed(bin_input_param);
-        free_if_needed(bin_output_param);
-        // Exiting with a failure ERROR CODE (== 1)
-        exit(EXIT_FAILURE);
-    }
+    // Checking binary requirements
+    check_requirements(&params);
 
-
-    // Printing params
-    print("** PARAMS **\n%-8s: %s\n%-8s: %s\n%-8s: %d\n",
-            "input",   bin_input_param,
-            "output",  bin_output_param,
-            "verbose", get_verbose_mode());
+    // Printing params if verbose mode is enabled
+    print_parameters(&params, get_verbose_mode());
 
     // Business logic must be implemented at this point
-
-    copy(bin_input_param, bin_output_param);
+    copy(params.input, params.output);
 
     // Freeing allocated data
-    free_if_needed(bin_input_param);
-    free_if_needed(bin_output_param);
-
+    free_if_needed(params.input);
+    free_if_needed(params.output);
 
     return EXIT_SUCCESS;
 }
