211211e4e2561c0add46e964848a45f017921295
s4ndalhat <winness.it@gmail.com>
2024-10-17 14:52:32 +0200

    feat: add ls like prototype & list content implementation

--- 01-syscalls_fs/include/ls_like.h
+++ 01-syscalls_fs/include/ls_like.h
@@ -9,5 +9,11 @@
 #include "print.h"
 #include "constants.h"
 
+void list_contents(const char *path);
+void show_permissions();
+void get_username();
+void get_groupname();
+void show_parameters();
+void show_atime();
 
 #endif // LS_LIKE_H
--- 01-syscalls_fs/src/ls_like.c
+++ 01-syscalls_fs/src/ls_like.c
@@ -1,1 +1,37 @@
 #include "ls_like.h"
+
+void list_contents(const char *path)
+{
+    DIR *dp;
+    struct dirent *ep;
+
+    dp = opendir(path);
+    if (dp != NULL) {
+        while (ep = readdir(dp))
+           puts(ep->d_name);
+        closedir(dp); 
+    }
+    else
+        perror("Couldn't open the directory");
+    return 0;
+}
+
+void show_permissions()
+{
+}
+
+void get_username()
+{
+}
+
+void get_groupname()
+{
+}
+
+void show_parameters()
+{
+}
+
+void show_atime()
+{
+}
