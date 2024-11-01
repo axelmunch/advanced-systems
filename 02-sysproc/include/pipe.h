7db7a9c46cfd10ae460b450465c54981a588641e
WRKT <winness.rakotozafy@outlook.fr>
2024-11-01 11:18:21 +0100

    fix(02-sysproc): clean pipe process fmode

--- 02-sysproc/include/pipe.h
+++ 02-sysproc/include/pipe.h
@@ -11,6 +11,9 @@
 int exec_ps(char *arg, ...);
 int exec_grep(char *arg, ...);
 int exec_echo(char *arg, ...);
+void handle_ps(int pipe_fd[2]);
+void handle_grep(int pipe_fd[2]);
+void handle_parent(pid_t grep_pid);
 void pipe_process();
 
 #endif // PIPE_H
--- 02-sysproc/src/pipe.c
+++ 02-sysproc/src/pipe.c
@@ -15,11 +15,90 @@ int exec_echo(char *arg, ...)
     return execlp("/bin/echo", "echo", arg, NULL);
 }
 
+/**
+ * @brief Handle the ps command
+ * @return void
+ * @param pipe_fd[2] Pipe file descriptors
+ *
+ * This function will close the read end of the pipe, duplicate the write end of the pipe to the STDOUT and close the write end of the pipe.
+ */
+void handle_ps(int pipe_fd[2])
+{
+    close(pipe_fd[0]);
+    dup2(pipe_fd[1], STDOUT_FILENO);
+    close(pipe_fd[1]);
+
+    if (exec_ps("aux") == -1)
+    {
+        print_error("[ERROR] exec_ps");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+/**
+ * @brief Handle the grep command
+ * @return void
+ * @param pipe_fd[2] Pipe file descriptors
+ *
+ * This function will close the write end of the pipe, duplicate the read end of the pipe to the STDIN and close the read end of the pipe.
+ */
+void handle_grep(int pipe_fd[2])
+{
+    close(pipe_fd[1]);
+    dup2(pipe_fd[0], STDIN_FILENO);
+    close(pipe_fd[0]);
+
+    int null_fd = open("/dev/null", O_WRONLY);
+    if (null_fd == -1)
+    {
+        print_error("[ERROR] open");
+        exit(EXIT_FAILURE);
+    }
+
+    dup2(null_fd, STDOUT_FILENO);
+    close(null_fd);
+
+    if (exec_grep("^root") == -1)
+    {
+        print_error("[ERROR] exec_grep");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+/**
+ * @brief Handle the parent process
+ * @return void
+ * @param grep_pid PID of the grep process
+ *
+ * This function will wait for the grep process to finish and check if the exit status is 0. If it is, it will print "root est connecté", otherwise it will print "root n'est pas connecté".
+ */
+void handle_parent(pid_t grep_pid)
+{
+    int status;
+    waitpid(grep_pid, &status, 0);
+
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+    {
+        print_generic(STDOUT_FILENO, "root est connecté\n");
+    }
+    else
+    {
+        print_generic(STDOUT_FILENO, "root n'est pas connecté\n");
+    }
+}
+
+/**
+ * @brief Pipe process
+ * @return void
+ *
+ * This function will create a pipe, fork two processes, one for the ps command and one for the grep command. The parent process will wait for the grep process to finish and print the result.
+ */
 void pipe_process()
 {
     int pipe_fd[2];
     pid_t ps_pid, grep_pid;
-    int null_fd, status;
 
     if (pipe(pipe_fd) == -1)
     {
@@ -35,14 +114,7 @@ void pipe_process()
     }
     else if (ps_pid == 0)
     {
-
-        close(pipe_fd[0]);
-        dup2(pipe_fd[1], STDOUT_FILENO);
-        if (exec_ps("eaux") == -1)
-        {
-            print_error("[ERROR] exec_ps");
-            exit(EXIT_FAILURE);
-        }
+        handle_ps_process(pipe_fd);
     }
     else
     {
@@ -54,41 +126,13 @@ void pipe_process()
         }
         else if (grep_pid == 0)
         {
-            close(pipe_fd[1]);
-            dup2(pipe_fd[0], STDIN_FILENO);
-            null_fd = open("/dev/null", O_WRONLY);
-
-            if (null_fd == -1)
-            {
-                print_error("[ERROR] open");
-                exit(EXIT_FAILURE);
-            }
-
-            dup2(null_fd, STDOUT_FILENO);
-            close(null_fd);
-
-            if (exec_grep("^root") == -1)
-            {
-                print_error("[ERROR] exec_grep");
-                exit(EXIT_FAILURE);
-            }
+            handle_grep(pipe_fd);
         }
         else
         {
             close(pipe_fd[0]);
             close(pipe_fd[1]);
-            waitpid(grep_pid, &status, 0);
-
-            if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
-            {
-                print_generic(STDOUT_FILENO, "root est connecté\n");
-            }
-            else
-            {
-                print_generic(STDOUT_FILENO, "root n'est pas connecté\n");
-            }
+            handle_parent(grep_pid);
         }
     }
 }
-
-// TO DO: add echo if all is OK
