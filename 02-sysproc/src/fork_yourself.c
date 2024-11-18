931f9b8fb65f47e1d08c6ff97c827f503a269c96
WRKT <winness.rakotozafy@outlook.fr>
2024-11-18 20:10:50 +0100

    feat(03-thread): add program skeleton

--- 02-sysproc/src/fork_yourself.c
+++ 02-sysproc/src/fork_yourself.c
@@ -1,5 +1,11 @@
 #include "fork_yourself.h"
 
+/**
+ * @brief Fork yourself
+ * @return int
+ *
+ * This function will fork a child process and print the PID and PPID of the child and the PID of the parent.
+ */
 int fork_yourself(void)
 {
     int status;
