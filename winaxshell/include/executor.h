1c12d725f867dffa490d1d77373ba747c6618214
WRKT <winness.rakotozafy@outlook.fr>
2025-01-07 21:56:52 +0100

    feat(winaxshell): add headers comments

--- winaxshell/include/executor.h
+++ winaxshell/include/executor.h
@@ -10,9 +10,34 @@
 #include "typedef.h"
 #include "parser.h"
 
+/**
+ * @brief Execute a single command
+ * @param args Command arguments
+ * @return int
+ */
 int execute_single_command(char **args);
+
+/**
+ * @brief Execute a pipe command
+ * @param left Left command
+ * @param right Right command
+ * @return int
+ */
 int execute_pipe_command(command_node_t *left, command_node_t *right);
+
+/**
+ * @brief Execute a command tree. This function recursively executes commands represented in a tree structure.
+ * It handles different types of commands (e.g., single commands, pipes) by traversing the tree and executing each node appropriately.
+ * @param node Command node
+ * @return int
+ */
 int execute_command_tree(command_node_t *node);
-void execute_command(char** args);
+
+/**
+ * @brief Execute a command. This function executes a command given its arguments.
+ * It is a wrapper around execute_single_command and other execution functions.
+ * @param args Command arguments
+ */
+void execute_command(char **args);
 
 #endif // COMMAND_H
--- winaxshell/include/parser.h
+++ winaxshell/include/parser.h
@@ -7,9 +7,30 @@
 #include "typedef.h"
 #include "constants.h"
 
+/**
+ * @brief Create a new command node
+ * @return command_node_t*
+ */
 command_node_t *create_command_node();
+
+/**
+ * @brief Get the operator type
+ * @param operator_str Operator string format
+ * @return operator_t enum type
+ */
 operator_t get_operator_type(const char *operator_str);
+
+/**
+ * @brief Free the command tree, including all nodes and arguments
+ * @param node Command node
+ */
 void free_command_tree(command_node_t* node);
+
+/**
+ * @brief Parse a command string and create a command tree
+ * @param input Command string
+ * @return command_tree_t*
+ */
 command_tree_t* parse_command(char* input);
 
 #endif // PARSER_H
