32f166c83971d22127d641ba852306098198bcaf
WRKT <winness.rakotozafy@outlook.fr>
2025-01-10 18:00:38 +0100

    fix(winaxshell): header comment executor

--- winaxshell/include/executor.h
+++ winaxshell/include/executor.h
@@ -14,7 +14,7 @@
 /**
  * @brief Execute a single command
  * @param args Command arguments
- * @return int
+ * @return int Exit status
  */
 int execute_single_command(char **args);
 
@@ -22,19 +22,22 @@ int execute_single_command(char **args);
  * @brief Execute a pipe command
  * @param left Left command
  * @param right Right command
- * @return int
+ * @return int Exit status
  */
 int execute_pipe_command(command_node_t *left, command_node_t *right);
 
 /**
- * @
+ * @brief Execute a background command
+ * @param node Command node
+ * @return int Exit status
  */
+int execute_background_command(command_node_t *node);
 
 /**
  * @brief Execute a command tree. This function recursively executes commands represented in a tree structure.
  * It handles different types of commands (e.g., single commands, pipes) by traversing the tree and executing each node appropriately.
  * @param node Command node
- * @return int
+ * @return int Exit status
  */
 int execute_command_tree(command_node_t *node);
 
@@ -42,6 +45,7 @@ int execute_command_tree(command_node_t *node);
  * @brief Execute a command. This function parses the input string and creates a command tree, then executes the command tree.
  * @param input User input string from the shell
  * @param command_tree Command tree to execute
+ * @return void
  */
 void execute_command(char* input, command_tree_t *command_tree);
 
